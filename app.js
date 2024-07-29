58ffe00d9f38b21d48408b569daf4f3b36684362
4rchie-r <archiereader@outlook.com>
2024-07-29 11:01:12 +0100

    First function created.

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ const PORT = 3000;
 app.use(express.static("public"));
 app.use(express.json());
 
+app.get('/api/recipes', async (req, res) => {
+  try {
+      const recipes = await getRecipes();
+      res.json(recipes);
+  } catch (error) {
+      res.status(500).json({ error: 'Failed to fetch recipes' });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
--- recipes.js
+++ recipes.js
@@ -4,7 +4,16 @@ import { v4 as uuidv4 } from "uuid";
 const fileName = "recipes.json";
 
 // GET ALL RECIPES
-export async function getRecipes() {}
+export async function getRecipes() {
+    try {
+        const data = await fs.readFile('recipes.json', 'utf8');
+        const recipes = JSON.parse(data);
+        return recipes;
+    } catch (error){
+        console.error('Error reading recipes.json:', error);
+        return [];
+    }
+}
 
 // GET A RECIPE BY ID
 export async function getRecipeByID(id) {}
